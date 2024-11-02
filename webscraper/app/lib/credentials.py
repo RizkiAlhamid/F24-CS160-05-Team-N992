9f94023ba0f33f1a538d3e0b5bc4649e452cfcd8
RizkiAlhamid <riskimifta@gmail.com>
2024-11-01 20:19:38 -0700

    use dotenv for credentials

--- webscraper/app/lib/credentials.py
+++ webscraper/app/lib/credentials.py
@@ -1,14 +1,16 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
 
 def get_env_variable(var_name: str, default: str = "") -> str:
     value = os.getenv(var_name, default)
     if value == "":
-        print("Environment variable is not defined var_name= ", var_name)
+        print(f"Environment variable is not defined var_name= {var_name}")
     return value
 
 YOUTUBE_API_KEY = get_env_variable("YOUTUBE_API_KEY")
 PERPLEXITY_API_KEY = get_env_variable("PERPLEXITY_API_KEY")
 OPENAI_API_KEY = get_env_variable("OPENAI_API_KEY")
 ANTHROPIC_API_KEY = get_env_variable("ANTHROPIC_API_KEY")
-
-
