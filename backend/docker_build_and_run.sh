8c64cae765a76d218985072850d8de1e7dfd4030
RizkiAlhamid <riskimifta@gmail.com>
2024-11-01 22:36:18 -0700

    dockerignore .env. docker_dev & docker_build_and_run scripts

--- backend/docker_build_and_run.sh
+++ backend/docker_build_and_run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Pull and run the Docker container
+# Build the Docker image
+docker build -t rizkialhamid/cs160-project-backend:latest .
+
+# Run the Docker container
 docker run -d --env-file .env -p 8080:8080 --name backend rizkialhamid/cs160-project-backend:latest
