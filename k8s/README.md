3ae0c82762b93c79467607ab3cc2155bff1f129b
RizkiAlhamid <riskimifta@gmail.com>
2024-11-03 17:38:14 -0800

    script to apply everything & readme

---
+++ k8s/README.md
@@ -0,0 +1,17 @@
+# Kubernetes Setup (IN PROGRESS)
+
+## Prerequisites
+- [Docker](https://docs.docker.com/get-docker/)
+- [Minikube](https://minikube.sigs.k8s.io/docs/start/)
+- [Kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
+
+## Steps
+1. Start minikube
+```bash
+minikube start
+```
+
+2. Use the up.sh script to create the deployments and services
+```bash
+./up.sh
+```
---
+++ k8s/up.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Function to apply the Kubernetes resources
+apply_k8s_resources() {
+    local path=$1
+    echo "Deploying resources from $path"
+    kubectl apply -f "$path"
+}
+
+# Deploy backend resources
+apply_k8s_resources "backend/mongo-uri-secret.yaml"
+apply_k8s_resources "backend/service.yaml"
+apply_k8s_resources "backend/deployment.yaml"
+
+# Deploy frontend resources
+#apply_k8s_resources "frontend/service.yaml"
+#apply_k8s_resources "frontend/deployment.yaml"
+
+# Deploy webscraper resources
+#apply_k8s_resources "webscraper/service.yaml"
+#apply_k8s_resources "webscraper/deployment.yaml"
+
+echo "Deployment complete."
